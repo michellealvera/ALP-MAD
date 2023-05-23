089d75eb968a3df300c85423be9283d540fbd05b
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-23 10:15:48 +0700

    Created SymbolHelper

---
+++ Pomodoro/Helpers/SymbolHelper.swift
@@ -0,0 +1,20 @@
+//
+//  SymbolHelper.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 23/05/23.
+//
+
+import Foundation
+
+struct SymbolHelper {
+    
+    let home = "home"
+    let clock = "clock"
+//    let person =
+//    let trashbin =
+//    let editPencil =
+//    let hotDrink =
+//    let bed = 
+    
+}
