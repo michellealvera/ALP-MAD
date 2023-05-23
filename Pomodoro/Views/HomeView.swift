74ff438c43e330de487f084156e9941c41c79253
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-23 16:07:30 +0700

    Created the three main tabviews
    Including MainView, HomeView, ProfileView, TimersView

---
+++ Pomodoro/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 23/05/23.
+//
+
+import SwiftUI
+
+// The HomeView contains the active timer and the playlist at the bottom, if we have time.
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Pomodoro/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 23/05/23.
+//
+
+import SwiftUI
+
+// The Main View is responsible to hold the navigation tabs to all other views.
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
