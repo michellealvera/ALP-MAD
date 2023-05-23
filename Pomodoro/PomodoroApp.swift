00f79b6488045680a5f33b3e955d50ae0b727759
michellealvera <89299208+michellealvera@users.noreply.github.com>
2023-05-23 08:16:29 +0700

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
