19c993cf1ec34fa056f58fc5663da4a62f017091
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-26 13:12:06 +0700

    Updated TimerView with ticks and such
    Adding the ticks in the clock
    Updating to basic colors first
    Added icons

--- Pomodoro/Views/TimerView.swift
+++ Pomodoro/Views/TimerView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 // The TimerView contains the active timer and the playlist at the bottom, if we have time.
 struct TimerView: View {
     
+    // MARK: Properties
+    @State var startAngle: Double = 0
+    // Since our to progress is 0.5
+    // 0.5 * 360 = 180
+    @State var toAngle: Double = 180
+    @State var startProgress: CGFloat = 0
+    @State var toProgress: CGFloat = 0.5
+    
     var body: some View {
         
         VStack{
@@ -51,53 +59,79 @@ struct TimerView: View {
     @ViewBuilder
     func SleepTimerSlider()->some View{
         
-        GeometryReader{proxy in
+        GeometryReader{ proxy in
             
             let width = proxy.size.width
             
             ZStack{
                 
+                
+                // MARK: Clock Design
+                ZStack{
+                    
+                    ForEach (1...60,id: \.self){index in
+                        Rectangle ()
+                            .fill(index % 5 == 0 ? .black : .gray)
+                        // Each hour will have big Line
+                        // 60/5 = 12
+                        // 12 Hours
+                            .frame (width: 2, height: index % 5 ==
+                        0 ? 15 : 7)
+                        // Setting into entire Circle
+                            .offset (y: (width - 60) / 2)
+                            .rotationEffect(.init(degrees:
+                             Double(index) * 6))
+                    }
+                    
+                }
+                
                 Circle ()
-                    .stroke(.black.opacity (0.06), lineWidth: 40)
+                    .stroke(.black.opacity(0.06), lineWidth: 40)
                 
                 Circle()
-                    .trim(from: 0, to: 0.5)
-                    .stroke(Color("Blue"), style:
+                    .trim(from: startProgress, to: toProgress)
+//                5F2EEA
+                    .stroke(Color(.blue), style:
                     StrokeStyle(lineWidth: 40, lineCap:
                             .round, lineJoin: .round))
                     .rotationEffect(.init(degrees: -90))
                 
                 // Slider Buttons
                 Image(systemName: "moon.fill")
-                    .font (.callout)
-                    .foregroundColor (Color ("Blue"))
-                    .frame (width: 30, height: 30).background(.white,in: Circle())
+                    .font(.callout)
+                    .foregroundColor(Color(.blue))
+                    .frame(width: 30, height: 30)
+                    .rotationEffect(.init(degrees: 90))
+                    .rotationEffect(.init(degrees: startAngle))
+                    .background(.white,in: Circle())
                 // Moving To Right & Rotating
                     .offset(x: width / 2)
                     .rotationEffect(.init(degrees: -90))
+                    .rotationEffect(.init(degrees: startAngle))
                 
                 
-                Image (systemName: "alarm")
-                    .font (.callout)
-                    .foregroundColor (Color ("Blue"))
-                    .frame (width: 30, height: 30) .background (.white,in: Circle())
+                Image(systemName: "alarm")
+                    .font(.callout)
+                    .foregroundColor(Color(.blue))
+                    .frame(width: 30, height: 30)
+                // Rotating Image Inside the circle
+                    .rotationEffect(.init(degrees: 90))
+                    .rotationEffect(.init(degrees: toAngle))
+                    .background(.white, in: Circle())
                 // Moving To Right & Rotating
-                    .offset(x: width / 2)
+                    .offset(x: width / 2) 
                     .rotationEffect(.init(degrees: -90))
                 // To the Current Angle
-                    .rotationEffect(.init(degrees: 0.0))
+                    .rotationEffect(.init(degrees: toAngle))
                 
             }
         }
-            .frame(width: screenBounds().width / 1.6, height:
+        .frame(width: screenBounds().width / 1.6, height:
         screenBounds().width / 1.6)
         
         
         
     }
-    
-    
-    
     
 }
 
