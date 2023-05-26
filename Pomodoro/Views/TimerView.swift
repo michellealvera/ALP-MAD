5c30c523c6fffdbb93734c40b20ee31cca113ca7
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-26 22:18:58 +0700

    Finished following to slider tutorial
    Kinda learned about the complex logic in the slider and translating the angles of the view into actual time.
    
    Had very few to little trouble learning. Hopefully all is well.

--- Pomodoro/Views/TimerView.swift
+++ Pomodoro/Views/TimerView.swift
@@ -48,6 +48,59 @@ struct TimerView: View {
                 }
             SleepTimerSlider()
                 .padding(.top, 50)
+            
+            Button {
+                
+            } label: {
+                Text("Start Sleep")
+                    .foregroundColor (.white)
+                    .padding(.vertical)
+                    .padding(.horizontal, 40)
+                    .background(Color(.blue),in: Capsule())
+            }
+            .padding(.top, 45)
+                
+            HStack(spacing: 25){
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Label {
+                        Text ("Bedtime")
+                            .foregroundColor(.black)
+                    } icon: {
+                        Image(systemName: "moon.fill")
+                            .foregroundColor(Color(.blue))
+                    }
+                    .font(.callout)
+                    
+                    Text(getTime(angle: startAngle).formatted(date: .omitted,
+                                                              time: .shortened))
+                    .font(.title2.bold())
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Label {
+                        Text ("Alarm")
+                            .foregroundColor(.black)
+                    } icon: {
+                        Image(systemName: "alarm")
+                            .foregroundColor(Color(.blue))
+                    }
+                    .font(.callout)
+                    
+                    Text(getTime(angle: toAngle).formatted(date: .omitted,
+                                                              time: .shortened))
+                    .font(.title2.bold())
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                
+                
+            }
+                .padding()
+                .background(.black.opacity(0.06), in: RoundedRectangle(cornerRadius: 15))
+                .padding(.top, 35)
+                
         }
         .padding()
         // Moving To Top Without Spacer
@@ -76,11 +129,24 @@ struct TimerView: View {
                         // 60/5 = 12
                         // 12 Hours
                             .frame (width: 2, height: index % 5 ==
-                        0 ? 15 : 7)
+                        0 ? 10 : 7)
                         // Setting into entire Circle
                             .offset (y: (width - 60) / 2)
                             .rotationEffect(.init(degrees:
                              Double(index) * 6))
+                    }
+                    
+                    
+                    // MARK: Clock Text
+                    let texts = [6,9,12,3]
+                    ForEach(texts.indices, id: \.self){index in
+                        Text("\(texts[index])")
+                            .font(.caption.bold() )
+                            .foregroundColor(.black)
+                            .rotationEffect(.init(degrees: Double(index) * -90))
+                            .offset(y: (width - 90) / 2)
+                        // 360/4 = 90
+                            .rotationEffect(.init(degrees: Double(index) * 90))
                     }
                     
                 }
@@ -88,13 +154,17 @@ struct TimerView: View {
                 Circle ()
                     .stroke(.black.opacity(0.06), lineWidth: 40)
                 
+                // Allowing Reverse Swipping
+                let reverseRotation = (startProgress > toProgress) ? -Double((1 - startProgress) * 360) : 0
+                
                 Circle()
-                    .trim(from: startProgress, to: toProgress)
+                    .trim(from: startProgress > toProgress ? 0 : startProgress, to: toProgress + (-reverseRotation / 360))
 //                5F2EEA
                     .stroke(Color(.blue), style:
                     StrokeStyle(lineWidth: 40, lineCap:
                             .round, lineJoin: .round))
                     .rotationEffect(.init(degrees: -90))
+                    .rotationEffect(.init(degrees: reverseRotation))
                 
                 // Slider Buttons
                 Image(systemName: "moon.fill")
@@ -102,12 +172,19 @@ struct TimerView: View {
                     .foregroundColor(Color(.blue))
                     .frame(width: 30, height: 30)
                     .rotationEffect(.init(degrees: 90))
-                    .rotationEffect(.init(degrees: startAngle))
+                    .rotationEffect(.init(degrees: -startAngle))
                     .background(.white,in: Circle())
                 // Moving To Right & Rotating
                     .offset(x: width / 2)
-                    .rotationEffect(.init(degrees: -90))
+//                    .rotationEffect(.init(degrees: -90))
                     .rotationEffect(.init(degrees: startAngle))
+                    .gesture(
+                        DragGesture()
+                            .onChanged ({ value in
+                                onDrag(value: value, fromSlider: true)
+                            })
+                    )
+                    .rotationEffect(.init(degrees: -90))
                 
                 
                 Image(systemName: "alarm")
@@ -116,22 +193,115 @@ struct TimerView: View {
                     .frame(width: 30, height: 30)
                 // Rotating Image Inside the circle
                     .rotationEffect(.init(degrees: 90))
-                    .rotationEffect(.init(degrees: toAngle))
+                    .rotationEffect(.init(degrees: -toAngle))
                     .background(.white, in: Circle())
                 // Moving To Right & Rotating
                     .offset(x: width / 2) 
-                    .rotationEffect(.init(degrees: -90))
+//                    .rotationEffect(.init(degrees: -90))
                 // To the Current Angle
                     .rotationEffect(.init(degrees: toAngle))
+                    .gesture(
+                        DragGesture()
+                            .onChanged ({ value in
+                                onDrag(value: value)
+                            })
+                    )
+                    .rotationEffect(.init(degrees: -90))
+                
+                // MARK: Hour Text
+                VStack(spacing: 6){
+                    Text ("\(getTimeDifference().0)hr")
+                        .font(.largeTitle.bold())
+                    Text("\(getTimeDifference().1)min")
+                        .foregroundColor(.gray)
+                }
                 
             }
         }
         .frame(width: screenBounds().width / 1.6, height:
         screenBounds().width / 1.6)
         
+    }
+    
+    func onDrag(value: DragGesture.Value, fromSlider: Bool = false) {
         
+        // MARK: Converting Translation into Angle
+        let vector = CGVector(dx: value.location.x, dy: value.location.y)
+        
+        // Removing the Button Radius
+        // Button Diameter = 30
+        // Radius = 15
+        let radians = atan2(vector.dy - 15, vector.dx - 15)
+        // Converting into Angle
+        var angle = radians * 180 / .pi
+        if angle < 0{angle = 360 + angle}
+        // Progress
+        let progress = angle / 360
+        
+        if fromSlider{
+            // Update From Values
+            self.startAngle = angle
+            self.startProgress = progress
+        }
+        else {
+            // Update To Values
+            self.toAngle = angle
+            self.toProgress = progress
+        }
         
     }
+    
+    func getTime(angle: Double)->Date{
+        
+        // 360 / 12 = 30
+        // 12 = Hours
+        let progress = angle / 30
+        // It will be 6.05
+        // 6 is Hour
+        // 0.5 is Minutes
+        let hour = Int(progress)
+        // Why 12
+        // Since we're going to update time for each 5 minutes not for each minute
+        // 6.1 = 5 minute
+        let remainder = (progress.truncatingRemainder(dividingBy: 1)
+                         * 12).rounded()
+        
+        var minute = remainder * 5
+        // This is because minutes are returning 60 (12*5)
+        // avoiding that to get perfect time
+        minute = (minute > 55 ? 55 : minute)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month,.day,.year], from: Date())
+          
+        let rawDay = (components.day ?? 0)
+        var day: Int = 0
+
+        if angle == toAngle{
+            day = rawDay + 1
+        }
+        else{
+            day = (startAngle > toAngle) ? rawDay : rawDay + 1
+        }
+        if let date = formatter.date(from: "\(components.year ?? 0)-\(components.month ?? 0)-\(day) \(hour == 24 ? 0 : hour):\(Int(minute)):00"){
+            return date 
+        }
+        return .init()
+    }
+    
+    func getTimeDifference()->(Int, Int){
+        
+        let calendar = Calendar.current
+        
+        let result = calendar.dateComponents([.hour,.minute], from: getTime(angle: startAngle), to: getTime(angle: toAngle))
+        
+        return (result.hour ?? 0, result.minute ?? 0)
+        
+    }
+    
     
 }
 
@@ -145,7 +315,7 @@ struct TimerView_Previews: PreviewProvider {
 extension View {
     
     // MARK: Screen Bounds Extension
-    func screenBounds ( )->CGRect {
+    func screenBounds()->CGRect {
         return UIScreen.main.bounds
     }
     
