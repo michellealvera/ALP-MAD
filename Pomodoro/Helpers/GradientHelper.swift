f881df1cd13b5fc17faa4c0e0bbd92b6ff49d1f2
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-28 20:01:15 +0700

    Created GradientHelper

---
+++ Pomodoro/Helpers/GradientHelper.swift
@@ -0,0 +1,27 @@
+//
+//  GradientHelper.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 28/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientHelper {
+    
+    static let violetFuschiaGradient = AngularGradient(
+        gradient: Gradient(colors: [Color("Fuschia 500"), Color("Violet 500")]),
+        center: .center,
+        startAngle: .degrees(340),
+        endAngle: .degrees(0)
+    )
+    
+    static let fuschiaVioletGradient = AngularGradient(
+        gradient: Gradient(colors: [Color("Violet 500"), Color("Fuschia 500")]),
+        center: .center,
+        startAngle: .degrees(340),
+        endAngle: .degrees(0)
+    )
+    
+}
