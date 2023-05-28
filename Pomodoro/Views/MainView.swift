899b3b2513c7b09b8dc385543d107c8e647876ff
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-28 14:53:20 +0700

    Creating TabView on Main

--- Pomodoro/Views/MainView.swift
+++ Pomodoro/Views/MainView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 // The Main View is responsible to hold the navigation tabs to all other views.
 struct MainView: View {
+    // State for holding the the users choise
+    @State private var selection: Tab = .timer
+    
+    // For tagging the tabs
+    enum Tab {
+        case timer
+        case tasks
+        case profile
+    }
+    
+    // Adding to Tabview and tagging them
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection) {
+            TimerView()
+                .tabItem {
+                    Label("Timer", systemImage: SymbolHelper().timer)
+                }
+                .tag(Tab.timer)
+
+            TasksView()
+                .tabItem{
+                    Label("Tasks", systemImage:
+                            SymbolHelper().tasks)
+                }
+                .tag(Tab.tasks)
+            
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: SymbolHelper().profile)
+                 }
+                .tag(Tab.profile)
+        }
     }
 }
 
