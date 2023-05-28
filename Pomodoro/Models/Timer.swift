dee97009066c567ce0e60a60f97a76f85b9184f3
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-28 19:32:08 +0700

    Adjusted the Timer Class

--- Pomodoro/Models/Timer.swift
+++ Pomodoro/Models/Timer.swift
@@ -7,35 +7,59 @@
 
 import Foundation
 
-// Study 
+// Study
+// Time is stored in minutes
 
-struct Timer {
+struct Timer: Identifiable, Hashable {
     
+    var id: UUID
     var name: String
     var isActiveTimer: Bool
     var studyDuration: Int
     var studySessions: Int
     var shortBreakDuration: Int
-    var shortBreakSessions: Int
     var longBreakDuration: Int
     
     init(
+        id: UUID = UUID(),
         name: String,
         isActiveTimer: Bool,
         studyDuration: Int,
         studySessions: Int,
         shortBreakDuration: Int,
-        shortBreakSessions: Int,
         longBreakDuration: Int
     ){
+        self.id = id
         self.name = name
         self.isActiveTimer = isActiveTimer
         self.studyDuration = studyDuration
         self.studySessions = studySessions
         self.shortBreakDuration = shortBreakDuration
-        self.shortBreakSessions = shortBreakSessions
         self.longBreakDuration = longBreakDuration
-        
     }
+    
+}
+
+extension Timer {
+    
+    static let sampleTimer: Timer =
+    Timer(
+        name: "DefaultTimer",
+        isActiveTimer: true,
+        studyDuration: 25,
+        studySessions: 2,
+        shortBreakDuration: 5,
+        longBreakDuration: 30
+    )
+    
+    static let emptyTimer: Timer =
+    Timer(
+        name: "EmptyTimer",
+        isActiveTimer: false,
+        studyDuration: 0,
+        studySessions: 0,
+        shortBreakDuration: 0,
+        longBreakDuration: 0
+    )
     
 }
