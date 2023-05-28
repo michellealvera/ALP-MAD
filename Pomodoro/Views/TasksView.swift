c24c414d89a47fb3bbed10ea117c516b98b6809d
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-28 14:53:04 +0700

    Renaming TimersView to TasksView

--- Pomodoro/Views/TasksView.swift
+++ Pomodoro/Views/TasksView.swift
@@ -1,20 +1,20 @@
 //
-//  TimersView.swift
+//  TasksView.swift
 //  Pomodoro
 //
-//  Created by MacBook Pro on 23/05/23.
+//  Created by MacBook Pro on 28/05/23.
 //
 
 import SwiftUI
 
-struct TimersView: View {
+struct TasksView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct TimersView_Previews: PreviewProvider {
+struct TasksView_Previews: PreviewProvider {
     static var previews: some View {
-        TimersView()
+        TasksView()
     }
 }
