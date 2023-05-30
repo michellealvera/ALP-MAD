7325b3fe74386824248811ee407c6a632d060106
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-30 16:10:30 +0700

    Maybe making multipicker view

---
+++ Pomodoro/Views/Timers/MultipickerView.swift
@@ -0,0 +1,24 @@
+//
+//  MultipickerView.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 30/05/23.
+//
+
+import SwiftUI
+
+// Max Hours: 99
+// Max Minutes: 59
+// Max Seconds: 59
+
+struct MultipickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultipickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultipickerView()
+    }
+}
