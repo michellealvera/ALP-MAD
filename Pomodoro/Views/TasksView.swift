f24b716f12ed42b856d2334e8e705c5a278582db
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-06-05 21:39:08 +0700

    Created TasksView
    - If the Array is Empty, then it will show empty.
    - If there is, it shows using TasksItemView
    - Added Floating Action Button but not functional yet

--- Pomodoro/Views/TasksView.swift
+++ Pomodoro/Views/TasksView.swift
@@ -8,13 +8,81 @@
 import SwiftUI
 
 struct TasksView: View {
+    
+    @EnvironmentObject private var modelData: ModelData
+//    @State private var activeTimerUUID: UUID = UUID()
+//    {
+//        didSet {
+//            if let activeTimer = modelData.activeUser.timers.first(where: { $0.isActiveTimer }) {
+//                activeTimerUUID = activeTimer.id
+//            } else {
+//                // Set a default UUID value if no active timer is found
+//                activeTimerUUID = UUID()
+//            }
+//        }
+//    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+            ZStack(alignment: .bottom) {
+                
+                if (!modelData.activeUser.timers.isEmpty){
+                    ScrollView {
+                        
+                        VStack{
+                            
+                            ForEach(modelData.activeUser.timers){ timerTasks in
+                                
+                                TasksItemView(
+                                    theTimerTask: timerTasks
+                                    // activeTimerUUID: $activeTimerUUID
+                                )
+                            }
+                        }
+                        
+                    }
+                    .navigationTitle("Tasks")
+                    .padding()
+                    
+                    
+                } else {
+                    
+                    VStack{
+                        Text("You have no tasks")
+                            .font(.title)
+                            .fontWeight(.bold)
+                    }
+                    .navigationTitle("Tasks")
+                    .padding()
+                    .frame(maxHeight: .infinity, alignment: .center)
+                    
+                }
+                
+                HStack{
+                    
+                    Spacer()
+                    
+                    VioletActionButton()
+                    
+                }.padding(.horizontal, 20)
+                
+            } //ZStack
+            
+        }
+//        .onAppear{
+//            self.activeTimerUUID = modelData.activeUser.getActiveTimer().id
+//        }
+        
     }
 }
 
 struct TasksView_Previews: PreviewProvider {
+    
+    static private let modelData = ModelData()
+    
     static var previews: some View {
         TasksView()
+            .environmentObject(modelData)
     }
 }
