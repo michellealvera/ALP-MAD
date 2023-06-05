d87f106ae5e8799f997ac41339cd8eb2074c256e
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-06-05 21:59:51 +0700

    Moved SessionType to its own file

---
+++ Pomodoro/Models/SessionType.swift
@@ -0,0 +1,14 @@
+//
+//  SessionType.swift
+//  Pomodoro
+//
+//  Created by MacBook Pro on 05/06/23.
+//
+
+import Foundation
+
+enum sessionType {
+    case Work
+    case Short_Break
+    case Long_Break
+}
