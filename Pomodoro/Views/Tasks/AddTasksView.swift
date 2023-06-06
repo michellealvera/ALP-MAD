97dad65a561ddbf4aef48800fa92bb740fb7ef5a
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-06-06 09:37:49 +0700

    Partial Update to AddTasksView
    Improved the look
    Made the input number only & only 2 characters
    Added several more states
    Added Violet Toggles

--- Pomodoro/Views/Tasks/AddTasksView.swift
+++ Pomodoro/Views/Tasks/AddTasksView.swift
@@ -6,64 +6,153 @@
 //
 
 import SwiftUI
+import Combine
 
 struct AddTasksView: View {
     
     // MARK: States
-    @State var quantity: Int = 10
+    
+//    @State var quantity: Int = 10
     @State var timerName:String = ""
+    
+    let maxMinute = 59
+    @State var minute: String = "1"
+    @State var shortBreak: Int = 5
+    @State var longBreak: Int = 10
+    
+    @State var enableLongBreak = false
+    @State var enableAutomaticStop = false
 
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 6){
+        NavigationStack{
             
-            Text("Timer Name")
-            TextField("Input Timer Name", text: $timerName)
+            Text("Add Timer")
+                .navigationBarTitleDisplayMode(.inline)
+                .font(.headline)
+                .fontWeight(.heavy)
+//                .frame(maxHeight: .infinity, alignment: .top)
+            
+            VStack(alignment: .leading, spacing: 6){
+                
+                Text("Timer Name")
+                    .fontWeight(.bold)
+                TextField("Input Timer Name", text: $timerName)
+                    .padding()
+                    .background(Color("Grayscale BG"))
+                    .cornerRadius(16)
+                
+                // Session Length - Hours, Minutes and Seconds
+                Text("Session Length")
+                    .fontWeight(.bold)
+                
+                HStack(alignment: .center){
+                    VStack {
+                         TextField("", text: $minute)
+                              .padding(.horizontal, 16)
+                              .multilineTextAlignment(.center)
+                              .font(.title)
+                              .keyboardType(.numberPad)
+                              .onReceive(Just(minute)) { _ in limitText(2) }
+                         Rectangle()
+                              .frame(height: 1)
+                              .foregroundColor(.black)
+                              .padding(.horizontal, 160)
+                    }
+                    
+                }.frame(maxWidth: .infinity, alignment: .center)
+                
+                // Make this into a component
+                // Pass in an individual State for each one
+                
+                // Short Break
+                Text("Short Break")
+                    .fontWeight(.bold)
+                NumberPicker(quantity: shortBreak, maxQuantity: maxMinute)
+                
+                // Long Break
+                HStack{
+                    Toggle(isOn: $enableLongBreak){
+                        Text("Long Break")
+                            .fontWeight(.bold)
+                    }
+                    .tint(Color("Violet 500"))
+                }
+            
+                NumberPicker(quantity: longBreak, maxQuantity: maxMinute)
+                
+    //            HStack{
+    //                Toggle(isOn: $enableAutomaticStop) {
+    //                    Text("Automatic Stop")
+    //                        .fontWeight(.bold)
+    //                }
+    //                .tint(Color("Violet 500"))
+    //            }
+                
+                VStack(alignment: .leading) {
+                    Text("Long Break Starts After")
+                        .fontWeight(.bold)
+                        
+                    
+                    HStack(alignment: .center){
+                        VStack {
+                             TextField("", text: $minute)
+                                  .padding(.horizontal, 16)
+                                  .font(.headline)
+                                  .keyboardType(.numberPad)
+                                  .onReceive(Just(minute)) { _ in limitText(2) }
+                             Rectangle()
+                                  .frame(height: 1)
+                                  .foregroundColor(.black)
+                                  .padding(.horizontal, 160)
+                        }
+                        
+                    }.frame(maxWidth: .infinity, alignment: .leading)
+                }
+
+                
+                
+                Button {
+                    saveNewTimer()
+                } label: {
+                    Text("Add Timer")
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Grayscale Off-White"))
+                }
+                .frame(maxWidth: .infinity)
                 .padding()
-                .background(Color("Grayscale BG"))
-                .cornerRadius(16)
-            
-            // Session Length - Hours, Minutes and Seconds
-            Text("Session Length")
-                .fontWeight(.bold)
-            
-            HStack{
-                // Three TextFields
+                .background(Color("Violet 500"))
+                .cornerRadius(8)
+                
+                
             }
-            
-            // Make this into a component
-            // Pass in an individual State for each one
-            
-            // Short Break
-            Text("Short Break")
-            NumberPicker(quantity: 5)
-            
-            // Long Break
-            Text("Long Break")
-            NumberPicker(quantity: 25)
-            
-            Text("Automatic Stop")
-                .fontWeight(.bold)
-            Text("Stops After")
-            
-            
-//            Button {
-//
-//            } label: {
-//                Text("Add Timer")
-//                    .fontWeight(.medium)
-//                    .foregroundColor(Color("Grayscale Off-White"))
-//            }
-//            .frame(maxWidth: .infinity)
-//            .padding()
-//            .background(Color("Violet 500"))
-//            .cornerRadius(8)
-            
-            
+//            .navigationTitle("Add Timer")
+            .padding()
         }
-        .padding()
+
+//        .frame(maxHeight: .infinity, alignment: .top)
         
     }
+    
+    func limitText(_ upper: Int) {
+        if minute.count > upper {
+            minute = String(minute.prefix(upper))
+        }
+        
+        let filtered = minute.filter { "0123456789".contains($0) }
+        if filtered != minute {
+            self.minute = filtered
+        }
+        
+    }
+    
+    func saveNewTimer(){
+        
+        let theMinute = Int(minute)
+        
+    }
+    
+    
 }
 
 struct AddTimerView_Previews: PreviewProvider {
